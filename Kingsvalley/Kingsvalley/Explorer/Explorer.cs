a518434dff24afffc872c43125b12983560faf2b
gekkegast84 <mick.rijneveld@live.nl>
2013-02-01 16:11:08 +0100

    Initial commit

---
+++ Kingsvalley/Kingsvalley/Explorer/Explorer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Kingsvalley
+{
+    public class Explorer
+    {
+
+        //Fields
+        private KingsValley game;
+        private Vector2 position;
+        private Texture2D texture;
+        private Rectangle rectangle;
+        private float speed = 1;
+        private AnimatedSprite state;
+
+        //Properties
+        public Vector2 Position
+        {
+            get { return this.position; }
+            set { 
+                    this.position = value;
+                    this.rectangle.X = (int)this.position.X;
+                    this.rectangle.Y = (int)this.position.Y;
+                }
+        }
+
+        public KingsValley Game
+        {
+            get { return this.game; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return this.texture; }
+        }
+
+        public Rectangle Rectangle
+        {
+            get { return this.rectangle; }
+        }
+
+        public float Speed
+        {
+            get { return this.speed; }
+        }
+
+        //Constructor
+        public Explorer(KingsValley game, Vector2 position)
+        {
+            this.game = game;
+            this.position = position;
+            this.texture = game.Content.Load<Texture2D>(@"explorer\explorer");
+            this.rectangle = new Rectangle((int)this.position.X,
+                                            (int)this.position.Y,
+                                                 this.texture.Width / 8,
+                                                 this.texture.Height);
+            this.state = new ExplorerWalkRight(this);
+        }
+
+        //Update
+        public void Update(GameTime gameTime)
+        {
+            this.state.Update(gameTime);
+            //Console.WriteLine(this.timer);
+
+        }
+
+        //Draw
+        public void Draw(GameTime gameTime)
+        {
+            this.state.Draw(gameTime);
+        }
+
+    }
+}
---
+++ Kingsvalley/Kingsvalley/KingsValley.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Kingsvalley
+{
+    public class KingsValley : Microsoft.Xna.Framework.Game
+    {
+        //FIELDS
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Explorer explorer;
+
+        //PROPERTIES
+        public SpriteBatch SpriteBatch
+        {
+            get { return this.spriteBatch; }
+        }
+
+        //CONSTRUCTOR
+        public KingsValley()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        //INITIALIZE METHOD
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            this.Window.Title = "KingsValley Beta";
+            this.graphics.PreferredBackBufferWidth = 640;
+            this.graphics.PreferredBackBufferHeight = 480;
+            this.graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+        //LOADCONTENT METHOD
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            this.explorer = new Explorer(this, new Vector2(100f, 300f));
+        }
+
+        //UNLOADCONTENT METHOD
+        protected override void UnloadContent()
+        {
+
+        }
+        //UPDATE
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            this.explorer.Update(gameTime);
+            base.Update(gameTime);
+        }
+        //DRAW
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(6,6,6));
+            this.spriteBatch.Begin();
+            this.explorer.Draw(gameTime);
+            this.spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
